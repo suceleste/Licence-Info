5a7c434cdf36d78743acf208575149fb430185af
suceleste <47870191+suceleste@users.noreply.github.com>
2024-02-19 14:26:59 +0100

    Delete TP Algo directory

--- TP Algo/TP04-Le_Jeu_du_Pendu/mots.py
+++
@@ -1,4 +0,0 @@
-__doc__ = 'Liste de mots pour le jeu du pendu.'
-__author__ = 'Tiago de Lima <tiago.delima@univ-artois.fr>'
-
-MOTS = ['aimable','barracuda','bavard','chercheur','cocorico','dernier','egalite','fanfaronner','garage','hasard','huluberlu','infini','moitie','nulle','present','rare','savant']
